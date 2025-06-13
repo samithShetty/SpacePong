a19403b31e87b5422131d6fa08226ff7ce501891
Samith Shetty <samithshetty0@gmail.com>
2025-06-12 21:15:23 -0400

    minor tweaks

--- Ball.gd
+++ Ball.gd
@@ -1,1 +1,13 @@
 extends RigidBody3D
+
+func bounce():
+	print("bounce")
+	linear_velocity *= -1
+
+func _on_body_entered(body: Node3D) -> void:
+	#if body is Paddle:
+		bounce()
+	
+
+func _on_area_entered(area: Area3D) -> void:
+	print("Score")
