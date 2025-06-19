e6296195b534ef0d8640a64b79b833b17dbfa56a
Samith Shetty <71335825+samithShetty@users.noreply.github.com>
2025-06-19 12:35:11 -0700

    Fixed score zones

--- Global.gd
+++ Global.gd
@@ -1,5 +1,7 @@
 extends Node
 
 @export var isOnTable: bool = false
+@export var ball: Ball
+var scores: Array[int] = [0,0]
 
 enum Player {P1, P2}
--- Main.gd
+++ Main.gd
@@ -1,12 +1,13 @@
 extends Node3D
 
+@export var ball: Ball
 
-var scores: Array[int] = [0,0]
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
-		
+
 func _on_score(player_id: int) -> void:
-	scores[player_id] += 1
-	print(scores)
+	Global.scores[player_id] += 1
+	print(Global.scores)
+	get_tree().reload_current_scene()
